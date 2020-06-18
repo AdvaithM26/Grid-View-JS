764d3f0911814d7614f1a296a7953dc35553c39f
Advaith Malka <65194982+AdvaithM26@users.noreply.github.com>
2020-06-18 12:51:23 -0400

    Add files via upload

---
+++ card-grid-view/card-grid-view.js
@@ -0,0 +1,18 @@
+/**
+ * IMPORTANT: JQuery must be loaded in before this script
+ * NOTE: class .pi-card must be added to the element with the class .col-lg-4 or col-lg-3 (whatever is the default)
+ * NOTE: add id #grid-4x3 on the compact view button and .grid-3x3 on the spaced view button
+ */
+
+
+let $card = $(".pi-card");
+
+$(".grid-4x3").on("click", () => {
+	$card.removeClass("col-lg-4");
+	$card.addClass("col-lg-3");
+});
+$(".grid-3x3").on("click", () => {
+	$card.removeClass("col-lg-3");
+	$card.addClass("col-lg-4");
+});
+
